13a441fdde254ccaaaf96201b28afdcc09687c0a
Warren Brown <warren@katapultmedia.com>
2020-02-26 08:35:59 -0600

    chore: added recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+recipes = Recipe.create(
+    [
+        {
+            title: 'Star Wars',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Lord',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Star Wars Episode IV',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Rings',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Wars',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Chaos',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Star Wars',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        },
+
+        {
+            title: 'Lord of the Rings',
+            description: 'my description',
+            prep_time: '1 hr',
+            ingredients: 'milk'
+        }
+
+
+    ]
+)
